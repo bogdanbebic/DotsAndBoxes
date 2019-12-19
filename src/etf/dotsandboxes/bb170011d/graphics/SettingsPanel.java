124fe2c679d67ca28aa9f0ed5c0d0600f44fda42
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2019-12-19 20:04:42 +0100

    Added AI difficulty choice in SettingsPanel

--- src/etf/dotsandboxes/bb170011d/graphics/SettingsPanel.java
+++ src/etf/dotsandboxes/bb170011d/graphics/SettingsPanel.java
@@ -3,6 +3,7 @@ package etf.dotsandboxes.bb170011d.graphics;
 import etf.dotsandboxes.bb170011d.engine.PlayerType;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SettingsPanel extends JPanel {
 
@@ -28,13 +29,45 @@ public class SettingsPanel extends JPanel {
         });
     }
 
+    private String [] aiDifficulties = { "Beginner", "Advanced", "Competitive" };
+
+    private JComboBox<String> ai1DifficultySelector = new JComboBox<>(this.aiDifficulties);
+    private JComboBox<String> ai2DifficultySelector = new JComboBox<>(this.aiDifficulties);
+    private JButton aiDifficultySelectionButton = new JButton("Select AI Difficulties");
+
+    // AI difficulty selection button action listener
+    {
+        this.aiDifficultySelectionButton.addActionListener(e -> {
+            String ai1Difficulty = (String)this.ai1DifficultySelector.getSelectedItem();
+            String ai2Difficulty = (String)this.ai2DifficultySelector.getSelectedItem();
+            // TODO: set AI difficulties to game
+            System.out.println("AI1: " + ai1Difficulty);
+            System.out.println("AI2: " + ai2Difficulty);
+        });
+    }
+
     // lays out components
     {
-        this.add(new JLabel("Player1 type:"));
-        this.add(this.player1TypeSelector);
-        this.add(new JLabel("Player2 type:"));
-        this.add(this.player2TypeSelector);
-        this.add(this.playerTypeSelectionButton);
+        this.setLayout(new GridLayout(2, 1));
+
+        JPanel playerTypeComponents = new JPanel();
+        // lays out player type components
+        playerTypeComponents.add(new JLabel("Player1 type:"));
+        playerTypeComponents.add(this.player1TypeSelector);
+        playerTypeComponents.add(new JLabel("Player2 type:"));
+        playerTypeComponents.add(this.player2TypeSelector);
+        playerTypeComponents.add(this.playerTypeSelectionButton);
+
+        // lays out AI difficulty components
+        JPanel aiDifficultiesComponents = new JPanel();
+        aiDifficultiesComponents.add(new JLabel("AI 1 difficulty:"));
+        aiDifficultiesComponents.add(this.ai1DifficultySelector);
+        aiDifficultiesComponents.add(new JLabel("AI 2 difficulty:"));
+        aiDifficultiesComponents.add(this.ai2DifficultySelector);
+        aiDifficultiesComponents.add(this.aiDifficultySelectionButton);
+
+        this.add(playerTypeComponents);
+        this.add(aiDifficultiesComponents);
     }
 
     public static void main(String ... args) {
