3fc87649d53ac1633eaf8a6c57848129ebb70738
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2020-01-04 20:09:16 +0100

    Implemented Player methods

--- src/etf/dotsandboxes/bb170011d/player/Player.java
+++ src/etf/dotsandboxes/bb170011d/player/Player.java
@@ -1,9 +1,28 @@
 package etf.dotsandboxes.bb170011d.player;
 
+import java.util.concurrent.SynchronousQueue;
+
 public class Player implements AbstractPlayer {
+
+    private SynchronousQueue<String> nextMove = new SynchronousQueue<>();
+
+    /** Blocks if no next move is known yet
+     * @return String representing the next move to be played
+     */
     @Override
     public String getNextMove() {
-        return null;
+        try {
+            return nextMove.take();
+        } catch (InterruptedException e) {
+            return null;
+        }
+    }
+
+    /** Might not have effect if called when no move is expected from the player
+     * @param nextMove String representing the next move to be played
+     */
+    public void setNextMove(String nextMove) {
+        this.nextMove.offer(nextMove);
     }
 
 }
